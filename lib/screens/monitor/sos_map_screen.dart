494aca4ffd4268b2750b2830a2afd6ab26abb914
shamanth kumar <sknavilehal@gmail.com>
2026-02-19 02:04:19 +0530

    Integrate flutter_map_cancellable_tile_provider for enhanced map functionality and update dependencies in pubspec.yaml and pubspec.lock

--- lib/screens/monitor/sos_map_screen.dart
+++ lib/screens/monitor/sos_map_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../../models/user_profile.dart';
@@ -406,6 +407,7 @@ class _SOSMapScreenState extends State<SOSMapScreen> {
                         urlTemplate:
                             'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                         userAgentPackageName: 'com.rrt.admin_site',
+                        tileProvider: CancellableNetworkTileProvider(),
                       ),
                       MarkerLayer(markers: markers),
                     ],
